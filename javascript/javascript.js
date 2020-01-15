80967af1535416eb7fdd98c17d864e99069e1799
Elke Burgers <elke.burgers@hotmail.com>
2020-01-15 11:58:13 -0300

    botao criar e campo de descricao fixo no final da tela, renderizacao na tela para mostrar lista de tarefas, storage das tarefas e manter o json carregado quando atualiza.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -10,7 +10,34 @@ let board = document.getElementById('board');
 // botao para adicionar tarefas
 let buttonAdd = document.getElementById('add');
 
+// passo 8 -  para adicionar descricao de nova tarefa:
+let inputAdd = document.getElementById('novaTarefa');
+
+// passo 10 - para nao zerar o array criado na renderizacao
+let listaTarefas = [];
+if (localStorage.getItem('listaTarefas')) {
+    listaTarefas = JSON.parse(localStorage.getItem('listaTarefas'))    
+}else{
+    // listaTarefas = []; - porque colocamos acima da funcao
+    localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));    
+}
+// testando agora no navegador, mesmo que atualize a pagina ele mantem as tarefas anteriores
+
+// para armazer as tarefas novas, primeiro cria o array e depois transforma em json (stringify - o parse faz o contrário): - cancelado no passo 10
+// let listaTarefas = [];
+// localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+
+//passo 9 - continuacao:
+mostrarNaTela(listaTarefas);
+
+// passo 5 - continuacao
 buttonAdd.onclick = function () {
+    // passo 8 - continuacao:
+    let valorDigitado = inputAdd.value;
+    // apos criar localStorage, para dar push da tarefa para ele:
+    listaTarefas.push(valorDigitado)
+
+    // passo 5 - continuacao
     // onclick = quando o botao eh acionado
     // function() = eh uma funcao anonima, nao pode ser chamada depois para reaproveitamento de codigo, entoa soh eh usada quando sabemos que nao precisamos reutilizar.    
 
@@ -23,24 +50,25 @@ buttonAdd.onclick = function () {
     let tarefa = document.createElement('div');
     // puxar os atributos da classe tarefa:
     tarefa.setAttribute('class', 'tarefa');
-    
+
     // criar a descricao/texto da tarefa:
     let titulo = document.createElement('div');
-    titulo.setAttribute('class','col=md-8');
+    titulo.setAttribute('class', 'col=md-8');
     // para o usuario colocar a descrição da tarefa:
     // estatico para teste:
-    titulo.textContent = "Essa é uma nova tarefa";
-    
+    // titulo.textContent = "Essa é uma nova tarefa"; - cancelado na etapa 8
+    titulo.textContent = valorDigitado;
+
     // criar o botao de check/feito:
     let buttonCheck = document.createElement('div');
-    buttonCheck.setAttribute('class','col-md-2');
-    
+    buttonCheck.setAttribute('class', 'col-md-2');
+
     // criar a imagem do botao check:
     let imgCheck = document.createElement('img');
     // abaixo puxo a classe que criei para controlar o tamanho da imagem:
-    imgCheck.setAttribute('class','botaoCheck');
+    imgCheck.setAttribute('class', 'botaoCheck');
     // puxando a imagem em si do botao:
-    imgCheck.setAttribute('src','images/botao_check.png');
+    imgCheck.setAttribute('src', 'images/botao_check.png');
 
     //para colocar o titulo, botao e imagem dentro do "tarefa":
     // abaixo: estou colocando o elemento botao dentro a imagem dele:
@@ -53,4 +81,40 @@ buttonAdd.onclick = function () {
     board.appendChild(tarefa);
 
     // testando agora no navegador, ao apertar o botao "adicionar tarefas", aparece um novo box de tarefa.
+
+    // passo 8 - continuação da funcao que puxa a tarefa nova apos - listaTarefas.push(valorDigitado)
+    // apos o comando abaixo o teste no navegador, com F12 traz no storage a informação carregada, mas F5 ainda apaga da memoria
+    localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas))
+    // para renderizar as tarefas (armazenar em definitivo e retornar na tela) - seguir passo 9
+}
+
+// passo 9 - renderizacao das tarefas
+// criar functions e copiar o box acima na gerarTarefa
+// item eh a informação que estamos armazenando (box tarefa nova)
+function mostrarNaTela(listaTarefas) {
+    for(let item of listaTarefas){
+        gerarTarefa(item)
+    }
+}
+
+function gerarTarefa(valorDigitado) {
+    let tarefa = document.createElement('div');
+    tarefa.setAttribute('class', 'tarefa');
+
+    let titulo = document.createElement('div');
+    titulo.setAttribute('class', 'col=md-8');
+    titulo.textContent = valorDigitado;
+
+    let buttonCheck = document.createElement('div');
+    buttonCheck.setAttribute('class', 'col-md-2');
+
+    let imgCheck = document.createElement('img');
+    imgCheck.setAttribute('class', 'botaoCheck');
+    imgCheck.setAttribute('src', 'images/botao_check.png');
+
+    buttonCheck.appendChild(imgCheck);
+    tarefa.appendChild(titulo);
+    tarefa.appendChild(buttonCheck);
+
+    board.appendChild(tarefa);
 }
